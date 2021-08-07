938b26126e474d6968b3114938cb437d9b3661c9
Hikari Genzo <leonxp@libero.it>
2021-08-07 23:29:35 +0200

    test

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const topic = require("./topic");
 const producer = require("./producer");
 const config = require("./config");
+const utils = require("./utils");
 
 async function main()
 {
     await topic.run();
-    await producer.run("Hello bro", config.kafka.topics[0].topic, 80, 1000);
+
+    //await producer.run("Hello bro", config.kafka.topics[0].topic, 80, 1000);
+    while(true){
+        await producer.run("Hello " + utils.makeRandomString(8), config.kafka.topics[0].topic, 1, 1);
+        await utils.sleep(1000);
+    }        
 }
 
 main();
--- utils.js
+++ utils.js
@@ -4,4 +4,16 @@ const sleep = function (ms) {
     });
 }  
 
+const makeRandomString = function (length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+}
+
 exports.sleep = sleep;
+exports.makeRandomString = makeRandomString;
